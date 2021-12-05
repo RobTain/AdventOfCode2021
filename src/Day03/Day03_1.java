2840fd7c19ed770c741852f9461a0d116fe6effd
Robert König <robsoft.softwaredevelopment@gmail.com>
2021-12-05 17:55:56 +0100

    Day 4_1 added

--- src/Day03/Day03_1.java
+++ src/Day03/Day03_1.java
@@ -3,7 +3,6 @@ package Day03;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class Day03_1 {
---
+++ src/Day04/Day04_1.java
@@ -0,0 +1,114 @@
+package Day04;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+public class Day04_1 {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("src/day04/input_1");
+		
+		ArrayList<String> input = new ArrayList<String>();
+	
+		try (Scanner sc = new Scanner(file)) {	
+			while (sc.hasNextLine()) {
+				input.add(sc.nextLine());
+			}
+		}	
+		
+		// check boards
+		int count = 0;
+		for (String string : input) {
+			if (string.equals("")) {
+				count++;
+			}
+		}
+		
+		ArrayList<Integer[][]> boards = new ArrayList<Integer[][]>();
+		
+		
+		// boards befüllen und in boards hauen
+		int size = 5;
+		Integer[][] board = new Integer[size][size];
+		int row = 0;
+		
+		for (int i = 2; i < input.size(); i++) {
+			String tmp = input.get(i);
+			if (tmp.equals("")) {
+				row = 0;
+				boards.add(board);
+				board = new Integer[size][size];
+				
+			} else {
+				tmp = tmp.replaceAll("\\\s+", " ");
+				if (tmp.charAt(0) == ' ') {
+					tmp = tmp.substring(1,tmp.length());
+				}
+				
+				String[] split = tmp.split(" ");
+				for (int j = 0; j < split.length; j++) {
+					board[j][row] = Integer.parseInt(split[j]);
+				}
+				row++;
+			}
+		}
+		
+		String[] winningNumbers = input.get(0).split(",");
+		boolean check = false;
+		int lastNumber = 0;
+		Integer[][] winner = new Integer[size][size];
+		
+		
+		for (int i = 0; i < winningNumbers.length; i++) {
+			if (check) {
+				break;
+			}
+			
+			int number = Integer.parseInt(winningNumbers[i]);
+			
+			for (Integer[][] b : boards) {
+				for (int j = 0; j < b.length; j++) {
+					for (int k = 0; k < b[j].length; k++) {
+						if (b[j][k] == number) {
+							b[j][k] = -1;
+						}
+					}
+				}
+			}
+			
+			// find winning board
+			
+			for (Integer[][] b : boards) {
+				for (int j = 0; j < b.length; j++) {
+					if ((b[j][0] == b[j][1] && b[j][1] == b[j][2] && b[j][2] == b[j][3] && b[j][3] == b[j][4]) || (b[0][j] == b[1][j] && b[1][j] == b[2][j] && b[2][j] == b[3][j] && b[3][j] == b[4][j])) {
+						winner = b;
+						lastNumber = number;
+						check = true;
+						break;
+					}
+				}
+				if (check) {
+					break;
+				}
+			}
+			
+		}
+		
+		
+		int result = 0;
+		for (int j = 0; j < winner.length; j++) {
+			for (int k = 0; k < winner[j].length; k++) {
+				if (winner[j][k] != -1) {
+					result += winner[j][k];
+				}
+			}
+		}
+		
+		System.out.println("Solution: " + (result*lastNumber));
+	}
+
+}
